335a6b32f04ade7ce72f3441471db95ee00e3791
Pranith Jain <pranithjain2006@gmail.com>
2026-06-24 23:34:52 +0530

    Correct syntax errors in test.c
    Fix syntax errors in test.c

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 20   // Error 1
+
+    printf("Sum = %d\n", a + b)  // Error 2
+
+    if(a < b {   // Error 3
+        printf("a is smaller\n");
+    }
+
+    return 0;
+}
